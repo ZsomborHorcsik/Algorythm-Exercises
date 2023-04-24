4c21c75793f31937deb3b292d82ff2eb03b44498
Zsombor Hörcsik <horcsik.zsombor88@gmail.com>
2023-04-24 15:32:26 +0200

    Add constructor to helper class

--- AlgorythmExcercises/Helpers/helper.cs
+++ AlgorythmExcercises/Helpers/helper.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 
 public class heleper
 {
-	public List<int> Generate_List_of_integers(int length)
+    public heleper()
+    {
+    }
+
+    public List<int> Generate_List_of_integers(int length)
 	{
         Random rand = new Random();
         List<int> result = new List<int>();
